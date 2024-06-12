d95e38db7b873962521c9ac0965d95aa6ff09c8a
Kuruyia <github@kuruyia.net>
2024-06-12 16:42:23 +0200

    feat: drop the person location column

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -8,6 +8,7 @@ import { HealthModule } from './health/health.module';
 import { PersonModule } from './person/person.module';
 import { V1Creation1718197500327 } from './migrations/1718197500327-V1_Creation';
 import { V2NullablePersonLocation1718202607833 } from './migrations/1718202607833-V2_NullablePersonLocation';
+import { V4DropPersonLocation1718203249447 } from './migrations/1718203249447-V4_DropPersonLocation';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { V2NullablePersonLocation1718202607833 } from './migrations/171820260783
         migrations: [
           V1Creation1718197500327,
           V2NullablePersonLocation1718202607833,
+          V4DropPersonLocation1718203249447,
         ],
         migrationsRun: true,
       }),
